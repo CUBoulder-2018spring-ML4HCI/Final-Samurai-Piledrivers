2e8245c221ffea194003b87c596826226c0e5bde
pianoz <rope7883@colorado.edu>
2018-04-09 14:04:09 -0600

    here is the class stuff.

--- control.py
+++ control.py
@@ -1,26 +1,6 @@
 import pyautogui
-import time
 import socket
 
-#
-# if char == 'y':
-#     pyautogui.press('down')
-#     pyautogui.keydown('down')
-#     pyautogui.keydown('right')
-#
-#     pyautogui.keyup('down')
-#     pyautogui.keyup('right')
-#     pyautogui.keydown('s')
-#     pyautogui.keydown('right')
-#
-#     pyautogui.keyup('s')
-#     pyautogui.keyup('right')
-#     print("yes")
-#             # press the left arrow key
-# elif event.char == 'p':  # OR  event.keycode == 50:
-#     pyautogui.press('s')
-
-# socket variables
 UDP_IP = "127.0.0.1"
 UDP_PORT_SEND = 6448
 UDP_PORT_RECE = 12000
@@ -29,47 +9,58 @@ sock_RECE = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 sock_RECE.bind((UDP_IP, UDP_PORT_RECE))
 
 
-
-
-def communicate():
-    print("number to send to wekinator")
-    text = input("Input number")
-    text = text.join([text[:5]] * 2)
-    print(text, " , sent.")
-    sock_SEND = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    sock_SEND.sendto(text.encode(), (UDP_IP, UDP_PORT_SEND))
-
 while(1):
 
-    #here are the classes, 1 is right, 2 is d, 3 is right, 4 is d as well
-    #communicate()
-
-    #listen for the wek
+    #here are the classes, 1,2,3,4,5,6,7,8: up,left,down,right,throw,quick,heavy,dodge
+    #listen for the wek, this will be replaced later by an in house ML algo.
     data, addr = sock_RECE.recvfrom(1024)  # buffer size is 1024 bytes
     #print(data, len(data),"/n")
     #print(data[20],data[21])
-    if data[20] == 64:
+    if data[20] == 63:
+        # class 1
+        pyautogui.keyDown('w')
+        pyautogui.keyUp('w')
+        print("up")
+
+    elif data[20] == 64:
         if data[21] == 0:
             #class 2
-            pyautogui.keyDown('d')
+            pyautogui.keyDown('a')
+            pyautogui.keyUp('a')
+            print("left")
         elif data[21] == 64:
             #class 3
-            pyautogui.keyUp('right')
+            pyautogui.keyDown('s')
+            pyautogui.keyUp('s')
+            print("down")
         elif data[21] == 128:
             #class 4
+            pyautogui.keyDown('d')
             pyautogui.keyUp('d')
+            print("right")
         elif data[21] == 160:
             #class 5
-            print("Action Unassigned, class 5")
+            pyautogui.keyDown('h')
+            pyautogui.keyUp('h')
+            print("throw")
         elif data[21] == 192:
             #class 6
-            print("action Unassigned, class 6")
+            pyautogui.keyDown('j')
+            pyautogui.keyUp('j')
+            print("quick")
         elif data[21] == 224:
             #class 7
-            print("action Unassigned, class 7")
-    else:
-        # class 1
-        pyautogui.keyDown('right')
+            pyautogui.keyDown('k')
+            pyautogui.keyUp('k')
+            print("heavy")
 
+    elif data[20] == 65:
+        if data[21] != 16:
+            #class 8
+            pyautogui.keyDown('l')
+            pyautogui.keyUp('l')
+            print("dodge")
+        elif data[21] == 16:
+            # class 9
+            print("standby")
 
-    time.sleep(1)
