0b686161fccc92204dedfb7296569318d7b29191
Matt Kaiser <mkaiser101@gmail.com>
2018-04-11 14:26:50 -0600

    up close grab and throw em

---
+++ combo scripts/Ryu/grabthrowCLOSE.py
@@ -0,0 +1,17 @@
+import pyautogui
+
+
+while(1):
+        pyautogui.keyDown('h')
+        pyautogui.keyDown('j')
+        pyautogui.keyUp('h')
+
+        pyautogui.keyDown('k')
+
+        pyautogui.keyUp('j')
+
+        pyautogui.keyDown('d')
+        pyautogui.keyUp('k')
+
+
+        pyautogui.keyUp('d')
