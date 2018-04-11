741caee1a3b3df603aee23d625c9c5d9707ef5db
Matt Kaiser <mkaiser101@gmail.com>
2018-04-11 14:41:34 -0600

    only works sometimes :/ needs more work
    on the first call it usually works but not for repeat calls might be fine for final version

---
+++ combo scripts/Ryu/dragonpunchuppercut.py
@@ -0,0 +1,18 @@
+import pyautogui
+
+
+
+while(1):
+
+        pyautogui.keyDown('k')
+        pyautogui.keyUp('k')
+        pyautogui.keyDown('j')
+        pyautogui.keyDown('k')
+        pyautogui.keyDown('s')
+        pyautogui.keyUp('j')
+
+
+        pyautogui.keyUp('k')
+
+
+        pyautogui.keyUp('s')
