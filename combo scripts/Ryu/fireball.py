cd21a3bfd188d44368817ad57d442a9a54634df0
Matt Kaiser <mkaiser101@gmail.com>
2018-04-12 16:04:24 -0600

    cleaned up script

--- combo scripts/Ryu/fireball.py
+++ combo scripts/Ryu/fireball.py
@@ -1,44 +1,16 @@
 import pyautogui
-import time
-#jks
+
+
 
 while(1):
-        #pyautogui.press(['down', 'right', 's'])
-        #time.sleep(1)
 
-        #pyautogui.typewrite('jks', interval=0.25)
-        #pyautogui.press('j')
-        #time.sleep(.001)
-        #pyautogui.press('s')
-        #time.sleep(.001)
         pyautogui.keyDown('j')
         pyautogui.keyDown('k')
 
         pyautogui.keyUp('j')
-        #time.sleep(.0001)
-        pyautogui.keyDown('s')
-        pyautogui.keyUp('k')
-        #time.sleep(.0001)S
-
-        pyautogui.keyUp('s')
-        #time.sleep(1)
-
-        '''pyautogui.keyDown('j')
-        pyautogui.keyUp('j')
-        time.sleep(.001)
-        pyautogui.keyDown('k')
-        pyautogui.keyUp('k')
-        time.sleep(.001)
 
         pyautogui.keyDown('s')
+        pyautogui.keyUp('k')
 
 
-        #pyautogui.keyDown('right')
-
-        #pyautogui.keyDown('d')
         pyautogui.keyUp('s')
-        time.sleep(.01)
-        time.sleep(1)
-
-        #pyautogui.hotkey('down', 'right', )
-        #pyautogui.typewrite('', interval=0.25)'''
