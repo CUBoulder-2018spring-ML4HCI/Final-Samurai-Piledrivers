aaa374df5f6982e04b65f1f0b3039dd3a501d499
Matt Kaiser <mkaiser101@gmail.com>
2018-04-12 16:05:03 -0600

    this fireball shoots from right to left while facing left

---
+++ combo scripts/Ryu/Lfireball.py
@@ -0,0 +1,16 @@
+import pyautogui
+
+
+
+while(1):
+
+        pyautogui.keyDown('l')
+        pyautogui.keyDown('k')
+
+        pyautogui.keyUp('l')
+
+        pyautogui.keyDown('s')
+        pyautogui.keyUp('k')
+
+
+        pyautogui.keyUp('s')
