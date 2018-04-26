82d614be93025993cf3cd7431aafd9090c35c33a
pianoz <rope7883@colorado.edu>
2018-04-26 17:09:18 -0600

    added some stuff.

--- UI things/control.py
+++ UI things/control.py
@@ -1,6 +1,7 @@
 import pyautogui
 import socket
 import time
+from microbit import *
 
 UDP_IP = "127.0.0.1"
 UDP_PORT_SEND = 6448
@@ -8,26 +9,38 @@ UDP_PORT_RECE = 12000
 
 sock_RECE = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 sock_RECE.bind((UDP_IP, UDP_PORT_RECE))
+outlist= [0]*9
+
+def checkhighest(outlist):
+    i=0
+    highest = 0
+    index = 0
+    while i < len(outlist):
+        if outlist[i] > highest:
+            highest = outlist[i]
+            index = i
+        i+=1
+    return index
 
 
 #M. Bison
 def PsychoCrusher():
-        pyautogui.keyDown('h')
-        time.sleep(2)
-        pyautogui.keyDown('k')
-        pyautogui.keyDown('s')
-        pyautogui.keyUp('h')
-        pyautogui.keyUp('k')
-        pyautogui.keyUp('s')
+    pyautogui.keyDown('h')
+    time.sleep(2)
+    pyautogui.keyDown('k')
+    pyautogui.keyDown('s')
+    pyautogui.keyUp('h')
+    pyautogui.keyUp('k')
+    pyautogui.keyUp('s')
 
 def ScissorKick():
-        pyautogui.keyDown('h')
-        time.sleep(2)
-        pyautogui.keyDown('k')
-        pyautogui.keyDown('d')
-        pyautogui.keyUp('h')
-        pyautogui.keyUp('k')
-        pyautogui.keyUp('d')
+    pyautogui.keyDown('h')
+    time.sleep(2)
+    pyautogui.keyDown('k')
+    pyautogui.keyDown('d')
+    pyautogui.keyUp('h')
+    pyautogui.keyUp('k')
+    pyautogui.keyUp('d')
 
 #Ryu
 def DragonPunch():
@@ -121,49 +134,74 @@ def control(character):
         # here are the classes, 1,2,3,4,,6,7,8: up,left,down,right,throw,quick,heavy,dodge
         # listen for the wek, this will be replaced later by an in house ML algo.
         data, addr = sock_RECE.recvfrom(1024)  # buffer size is 1024 bytes
-        # print(data, len(data),"/n")
-        # print(data[20],data[21])
-        if data[20] == 63:
-            # class 1
-            pyautogui.keyDown('w')
-            pyautogui.keyUp('w')
-            print("up")
+        print(data, len(data),"/n")
+        print(data[30],data[31],data[32],data[33],data[34],data[35],data[26],data[27],data[28],data[29])
 
-        elif data[20] == 64:
-            if data[21] == 0:
-                # class 2
-                pyautogui.keyDown('a')
-                pyautogui.keyUp('a')
-                print("left")
-            elif data[21] == 64:
-                # class 3
-                pyautogui.keyDown('s')
-                pyautogui.keyUp('s')
-                print("down")
-            elif data[21] == 128:
-                # class 4
-                pyautogui.keyDown('d')
-                pyautogui.keyUp('d')
-                print("right")
-            elif data[21] == 160:
-                # class 5
-                pyautogui.keyDown('h')
-                pyautogui.keyUp('h')
-                print("throw")
-            elif data[21] == 192:
-                # class 6
-                pyautogui.keyDown('j')
-                pyautogui.keyUp('j')
-                print("quick")
-            elif data[21] == 224:
-                # class 7
-                comboInterp(character, 1)
+        data9 = [data[30],data[31],data[32],data[33],data[34],data[35],data[26],data[27],data[28]]
+        index = checkhighest(data9)
 
-        elif data[20] == 65:
-            if data[21] != 16:
-                # class 8
-                comboInterp(character, 2)
-            elif data[21] == 16:
-                # class 9
-                print("standby")
+        if index == 0:
+            pass
+        elif index == 1:
+            pass
+        elif index == 2:
+            pass
+        elif index == 3:
+            pass
+        elif index == 4:
+            pass
+        elif index == 5:
+            pass
+        elif index == 6:
+            pass
+        elif index == 7:
+            pass
+        elif index == 8:
+            pass
 
+        
+
+        #
+        # if data[20] == 63:
+        #     # class 1
+        #     pyautogui.keyDown('w')
+        #     pyautogui.keyUp('w')
+        #     print("up")
+        #
+        # elif data[20] == 64:
+        #     if data[21] == 0:
+        #         # class 2
+        #         pyautogui.keyDown('a')
+        #         pyautogui.keyUp('a')
+        #         print("left")
+        #     elif data[21] == 64:
+        #         # class 3
+        #         pyautogui.keyDown('s')
+        #         pyautogui.keyUp('s')
+        #         print("down")
+        #     elif data[21] == 128:
+        #         # class 4
+        #         pyautogui.keyDown('d')
+        #         pyautogui.keyUp('d')
+        #         print("right")
+        #     elif data[21] == 160:
+        #         # class 5
+        #         pyautogui.keyDown('h')
+        #         pyautogui.keyUp('h')
+        #         print("throw")
+        #     elif data[21] == 192:
+        #         # class 6
+        #         pyautogui.keyDown('j')
+        #         pyautogui.keyUp('j')
+        #         print("quick")
+        #     elif data[21] == 224:
+        #         # class 7
+        #         comboInterp(character, 1)
+        #
+        # elif data[20] == 65:
+        #     if data[21] != 16:
+        #         # class 8
+        #         comboInterp(character, 2)
+        #     elif data[21] == 16:
+        #         # class 9
+        #         print("standby")
